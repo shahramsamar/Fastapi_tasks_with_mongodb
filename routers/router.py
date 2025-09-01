e48f3b8d0de34ea1406ceeecc8306a99099fa5f0
shahram <shahramsamar2010@gmail.com>
2025-09-01 15:25:16 +0330

    list of tasks

--- routers/router.py
+++ routers/router.py
@@ -5,10 +5,18 @@ from configurations.database import tasks_collection
 
 MyRuter = APIRouter()
 
+
+
+
+@MyRuter.get("/tasks")
+async def get_tasks():
+    tasks = list(tasks_collection.find({},{"_id":0}))  
+    return {"tasks":tasks}  
+
+
+
 @MyRuter.post("/add_task")
 async def add_task(task:Tasks):
     task_data = task.dict()
     tasks_collection.insert_one(task_data)
     return {"message":"Task added successfully"}
-
-    
