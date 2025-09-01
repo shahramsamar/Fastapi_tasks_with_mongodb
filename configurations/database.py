6f55f234f8e2c7c98581a7eb09b9c8411edf8313
shahram <shahramsamar2010@gmail.com>
2025-09-01 13:21:32 +0330

    create setting for database

---
+++ configurations/database.py
@@ -0,0 +1,17 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+uri ="mongodb://localhost:27017/?directConnection=true"
+
+clinet = MongoClient(uri,server_api=ServerApi("1"))
+
+
+try:
+    clinet.admin.command("ping")
+    print("Pinged your deployment.You successfilly connected to MongoDb!!!!!")
+except Exception as e:
+    print(e)
+
+db = clinet.tasks
+tasks_collection = db['task']     
